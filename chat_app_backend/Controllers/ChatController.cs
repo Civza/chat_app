e0c82450f319c86aa9e23077feea250ed8762c61
Raul Valdes <civza696@outlook.com>
2024-09-12 09:38:35 -0700

    started backkend develpment in c#

---
+++ chat_app_backend/Controllers/ChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chat_app_backend.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase {
+        [HttpPost("messages")]
+        public IActionResult Index() {
+
+        }
+    }
+}
